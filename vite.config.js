991e0b346434637e0b5af92b601b30a060469a49
Jath Palasubramaniam <jathpala@gmail.com>
2023-09-23 19:27:47 +1000

    Updated to Svelte 4

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import VitePluginRestart from 'vite-plugin-restart'
 import path from 'path'
 
-// No idea why this is needed. The default export of vite-plugin-restart looks
-// to be the function but it doesn't work when imported. Need to access the
-// 'default' key from the imported object instead
-const ViteRestart = VitePluginRestart.default
+const ViteRestart = VitePluginRestart
 
 /** @type {import('vite').UserConfig} */
 const config = {
